db1362efa59871b235bd6ac9352f6729685cf21d
shuvo794 <shuvohossain794@gmail.com>
2023-12-29 12:48:02 +0600

    added icon

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,13 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
