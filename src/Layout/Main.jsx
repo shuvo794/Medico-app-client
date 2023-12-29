ea6169369690bc2da9b19bce1c9570525d08bcfe
shuvo794 <shuvohossain794@gmail.com>
2023-12-29 20:38:49 +0600

    send solve code

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,12 @@
 
 import { Outlet } from 'react-router-dom'
 import Footer from '../Shared/Footer/Footer'
+import NavBar from '../Shared/Navbar/NavBar'
 
 function Main() {
   return (
     <div>
+      <NavBar></NavBar>
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
