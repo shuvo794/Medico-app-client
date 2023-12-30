fdc3ee03cc52615647bf4d47f0e53d9bb341f318
shuvo794 <shuvohossain794@gmail.com>
2023-12-30 19:51:05 +0600

    send

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,10 @@
 
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Footer from '../Shared/Footer/Footer'
 import NavBar from '../Shared/Navbar/NavBar'
 
 function Main() {
+   const location = useLocation();
   return (
     <div>
       <NavBar></NavBar>
