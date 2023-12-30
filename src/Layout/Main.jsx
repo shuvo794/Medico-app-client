ea35e79bfd9437032a36c18e13ce73c9473949fc
shuvo794 <shuvohossain794@gmail.com>
2023-12-30 19:53:18 +0600

    send

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,20 +1,18 @@
-
-import { Outlet, useLocation } from 'react-router-dom'
-import Footer from '../Shared/Footer/Footer'
-import NavBar from '../Shared/Navbar/NavBar'
+import { Outlet, useLocation } from "react-router-dom";
+import Footer from "../Shared/Footer/Footer";
+import NavBar from "../Shared/Navbar/NavBar";
 
 function Main() {
   const location = useLocation();
-  const noHeaderFooter = location.pathname.includes('login') || location.pathname.includes('signup')
+  const noHeaderFooter =
+    location.pathname.includes("login") || location.pathname.includes("signup");
   return (
     <div>
       <NavBar></NavBar>
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
-    
-
-  )
+  );
 }
 
-export default Main
+export default Main;
