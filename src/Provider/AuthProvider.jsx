23b88882714b8b5974a987dbb6474aea2de76b0b
shuvo794 <shuvohossain794@gmail.com>
2023-12-31 11:48:34 +0600

    firebase setup

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,10 +1,22 @@
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
+
 
 const AuthContext = createContext(null);
-
+const auth = getAuth(app);
 function AuthProvider({ children }) {
     const [user, setuser] = useState(null);
     const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, currentUser => {
+      setuser(currentUser);
+      console.log('currentUser', currentUser);
+      setLoading(false);
+    })
+  },[])
+  
     const appInfo = {
         user,
         loading
